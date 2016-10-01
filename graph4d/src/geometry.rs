fb22ebc3f6c59ac4b4548a5bb9868ac05e2e565c
Bartłomiej Kamiński <fizyk20@gmail.com>
2016-10-01 22:59:12 +0200

    More tests

--- graph4d/src/geometry.rs
+++ graph4d/src/geometry.rs
@@ -83,6 +83,10 @@ impl Matrix {
             ]
         }
     }
+
+    pub fn from_array(arr: [[f64; 5]; 5]) -> Matrix {
+        Matrix { coords: arr }
+    }
 }
 
 impl<T: Borrow<Vector>> ops::Add<T> for Vector {
@@ -325,7 +329,7 @@ impl<'b> ops::Mul<&'b Vector> for Matrix {
 
 #[cfg(test)]
 mod test {
-    use super::Vector;
+    use super::{Vector, Matrix};
 
     #[test]
     fn test_add_vectors() {
@@ -336,6 +340,191 @@ mod test {
         assert_eq!(c.y(), 4.0);
         assert_eq!(c.z(), 10.0);
         assert_eq!(c.w(), 10.0);
+    }
+
+    #[test]
+    fn test_sub_vectors() {
+        let a = Vector::new(0.0, 1.0, 2.0, 3.0);
+        let b = Vector::new(2.0, 3.0, 8.0, 7.0);
+        let c = a - &b;
+        assert_eq!(c.x(), -2.0);
+        assert_eq!(c.y(), -2.0);
+        assert_eq!(c.z(), -6.0);
+        assert_eq!(c.w(), -4.0);
+    }
+
+    #[test]
+    fn test_mul_vec_f64() {
+        let a = Vector::new(0.0, 1.0, 2.0, 3.0);
+        let c = a * 3.0;
+        assert_eq!(c.x(), 0.0);
+        assert_eq!(c.y(), 3.0);
+        assert_eq!(c.z(), 6.0);
+        assert_eq!(c.w(), 9.0);
+    }
+
+    #[test]
+    fn test_div_vec_f64() {
+        let a = Vector::new(0.0, 1.0, 2.0, 3.0);
+        let c = a / 2.0;
+        assert_eq!(c.x(), 0.0);
+        assert_eq!(c.y(), 0.5);
+        assert_eq!(c.z(), 1.0);
+        assert_eq!(c.w(), 1.5);
+    }
+
+    #[test]
+    fn test_dot_product() {
+        let a = Vector::new(0.0, 1.0, 2.0, 3.0);
+        let b = Vector::new(2.0, 3.0, 8.0, 7.0);
+        let c = a.dot(b);
+        assert_eq!(c, 40.0);
+    }
+
+    #[test]
+    fn test_add_matrices() {
+        let a = Matrix::from_array([[0.0, 1.0, 2.0, 3.0, 4.0],
+                                    [4.0, 3.0, 2.0, -3.0, 5.0],
+                                    [-2.0, 8.0, -1.0, 0.0, 4.0],
+                                    [1.0, 6.0, 3.0, 2.0, 2.0],
+                                    [2.0, 3.0, 3.0, 1.0, 0.0]]);
+        let b = Matrix::identity();
+        let c = a + b;
+        assert_eq!(c.coords[0][0], 1.0);
+        assert_eq!(c.coords[0][1], 1.0);
+        assert_eq!(c.coords[0][2], 2.0);
+        assert_eq!(c.coords[0][3], 3.0);
+        assert_eq!(c.coords[0][4], 4.0);
+        assert_eq!(c.coords[1][0], 4.0);
+        assert_eq!(c.coords[1][1], 4.0);
+        assert_eq!(c.coords[1][2], 2.0);
+        assert_eq!(c.coords[1][3], -3.0);
+        assert_eq!(c.coords[1][4], 5.0);
+        assert_eq!(c.coords[2][0], -2.0);
+        assert_eq!(c.coords[2][1], 8.0);
+        assert_eq!(c.coords[2][2], 0.0);
+        assert_eq!(c.coords[2][3], 0.0);
+        assert_eq!(c.coords[2][4], 4.0);
+        assert_eq!(c.coords[3][0], 1.0);
+        assert_eq!(c.coords[3][1], 6.0);
+        assert_eq!(c.coords[3][2], 3.0);
+        assert_eq!(c.coords[3][3], 3.0);
+        assert_eq!(c.coords[3][4], 2.0);
+        assert_eq!(c.coords[4][0], 2.0);
+        assert_eq!(c.coords[4][1], 3.0);
+        assert_eq!(c.coords[4][2], 3.0);
+        assert_eq!(c.coords[4][3], 1.0);
+        assert_eq!(c.coords[4][4], 1.0);
+    }
+
+    #[test]
+    fn test_sub_matrices() {
+        let a = Matrix::from_array([[0.0, 1.0, 2.0, 3.0, 4.0],
+                                    [4.0, 3.0, 2.0, -3.0, 5.0],
+                                    [-2.0, 8.0, -1.0, 0.0, 4.0],
+                                    [1.0, 6.0, 3.0, 2.0, 2.0],
+                                    [2.0, 3.0, 3.0, 1.0, 0.0]]);
+        let b = Matrix::identity();
+        let c = a - b;
+        assert_eq!(c.coords[0][0], -1.0);
+        assert_eq!(c.coords[0][1], 1.0);
+        assert_eq!(c.coords[0][2], 2.0);
+        assert_eq!(c.coords[0][3], 3.0);
+        assert_eq!(c.coords[0][4], 4.0);
+        assert_eq!(c.coords[1][0], 4.0);
+        assert_eq!(c.coords[1][1], 2.0);
+        assert_eq!(c.coords[1][2], 2.0);
+        assert_eq!(c.coords[1][3], -3.0);
+        assert_eq!(c.coords[1][4], 5.0);
+        assert_eq!(c.coords[2][0], -2.0);
+        assert_eq!(c.coords[2][1], 8.0);
+        assert_eq!(c.coords[2][2], -2.0);
+        assert_eq!(c.coords[2][3], 0.0);
+        assert_eq!(c.coords[2][4], 4.0);
+        assert_eq!(c.coords[3][0], 1.0);
+        assert_eq!(c.coords[3][1], 6.0);
+        assert_eq!(c.coords[3][2], 3.0);
+        assert_eq!(c.coords[3][3], 1.0);
+        assert_eq!(c.coords[3][4], 2.0);
+        assert_eq!(c.coords[4][0], 2.0);
+        assert_eq!(c.coords[4][1], 3.0);
+        assert_eq!(c.coords[4][2], 3.0);
+        assert_eq!(c.coords[4][3], 1.0);
+        assert_eq!(c.coords[4][4], -1.0);
+    }
+
+    #[test]
+    fn test_mul_matrix_identity() {
+        let a = Matrix::from_array([[0.0, 1.0, 2.0, 3.0, 4.0],
+                                    [4.0, 3.0, 2.0, -3.0, 5.0],
+                                    [-2.0, 8.0, -1.0, 0.0, 4.0],
+                                    [1.0, 6.0, 3.0, 2.0, 2.0],
+                                    [2.0, 3.0, 3.0, 1.0, 0.0]]);
+        let b = Matrix::identity();
+        let c = &a * &b;
+        let d = &b * &a;
+        for i in 0..5 {
+            for j in 0..5 {
+                assert_eq!(c.coords[i][j], a.coords[i][j]);
+                assert_eq!(d.coords[i][j], a.coords[i][j]);
+            }
+        }
+    }
+
+    #[test]
+    fn test_mul_matrices() {
+        let a = Matrix::from_array([[0.0, 1.0, 2.0, 3.0, 4.0],
+                                    [4.0, 3.0, 2.0, -3.0, 5.0],
+                                    [-2.0, 8.0, -1.0, 0.0, 4.0],
+                                    [1.0, 6.0, 3.0, 2.0, 2.0],
+                                    [2.0, 3.0, 3.0, 1.0, 0.0]]);
+        let b = Matrix::from_array([[-7.0, 1.0, 2.0, 3.0, 2.0],
+                                    [4.0, 6.0, 2.0, 3.0, 2.0],
+                                    [-2.0, 8.0, 1.0, 0.0, 4.0],
+                                    [1.0, 3.0, 3.0, 4.0, 2.0],
+                                    [2.0, 1.0, 3.0, 1.0, 3.0]]);
+        let c = a * b;
+        assert_eq!(c.coords[0][0], 11.0);
+        assert_eq!(c.coords[0][1], 35.0);
+        assert_eq!(c.coords[0][2], 25.0);
+        assert_eq!(c.coords[0][3], 19.0);
+        assert_eq!(c.coords[0][4], 28.0);
+        assert_eq!(c.coords[1][0], -13.0);
+        assert_eq!(c.coords[1][1], 34.0);
+        assert_eq!(c.coords[1][2], 22.0);
+        assert_eq!(c.coords[1][3], 14.0);
+        assert_eq!(c.coords[1][4], 31.0);
+        assert_eq!(c.coords[2][0], 56.0);
+        assert_eq!(c.coords[2][1], 42.0);
+        assert_eq!(c.coords[2][2], 23.0);
+        assert_eq!(c.coords[2][3], 22.0);
+        assert_eq!(c.coords[2][4], 20.0);
+        assert_eq!(c.coords[3][0], 17.0);
+        assert_eq!(c.coords[3][1], 69.0);
+        assert_eq!(c.coords[3][2], 29.0);
+        assert_eq!(c.coords[3][3], 31.0);
+        assert_eq!(c.coords[3][4], 36.0);
+        assert_eq!(c.coords[4][0], -7.0);
+        assert_eq!(c.coords[4][1], 47.0);
+        assert_eq!(c.coords[4][2], 16.0);
+        assert_eq!(c.coords[4][3], 19.0);
+        assert_eq!(c.coords[4][4], 24.0);
+    }
+
+    #[test]
+    fn test_mul_matrix_vector() {
+        let a = Matrix::from_array([[0.0, 1.0, 2.0, 3.0, 4.0],
+                                    [4.0, 3.0, 2.0, -3.0, 5.0],
+                                    [-2.0, 8.0, -1.0, 0.0, 4.0],
+                                    [1.0, 6.0, 3.0, 2.0, 2.0],
+                                    [2.0, 3.0, 3.0, 1.0, 0.0]]);
+        let b = Vector::new(2.0, -2.0, 1.0, 3.0);
+        let c = a * &b;
+        assert_eq!(c.coords[0], 13.0);
+        assert_eq!(c.coords[1], 0.0);
+        assert_eq!(c.coords[2], -17.0);
+        assert_eq!(c.coords[3], 1.0);
+        assert_eq!(c.coords[4], 4.0);
     }
 
 }
