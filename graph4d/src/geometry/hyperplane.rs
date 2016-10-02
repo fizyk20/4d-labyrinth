a532f4887792f623589ef2c016a47e6fcfd0c054
Bartłomiej Kamiński <fizyk20@gmail.com>
2016-10-02 12:30:29 +0200

    Added Hyperplane object

---
+++ graph4d/src/geometry/hyperplane.rs
@@ -0,0 +1,19 @@
+use super::Vector;
+
+pub struct Hyperplane {
+    normal: Vector,
+    param: f64
+}
+
+impl Hyperplane {
+    pub fn new(v: Vector, p: f64) -> Hyperplane {
+        Hyperplane {
+            normal: v,
+            param: p
+        }
+    }
+
+    pub fn dot(&self, v: &Vector) -> f64 {
+        self.normal.dot(v) + self.param
+    }
+}
--- graph4d/src/geometry/mod.rs
+++ graph4d/src/geometry/mod.rs
@@ -1,5 +1,7 @@
 pub mod vector;
 pub mod matrix;
+pub mod hyperplane;
 
 pub use self::vector::Vector;
 pub use self::matrix::Matrix;
+pub use self::hyperplane::Hyperplane;
